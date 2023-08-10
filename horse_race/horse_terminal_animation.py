8d08fe03a5ef0d7e1c6d78c3ddd751a1966e1521
tnosek <tnosek@student.wszib.edu.pl>
2023-08-10 13:22:09 +0200

    formatting changes

--- horse_race/horse_terminal_animation.py
+++ horse_race/horse_terminal_animation.py
@@ -1,14 +1,17 @@
-from horse_art import running_horses
-from time import sleep
 import os
+from time import sleep
+
+from horse_art import running_horses
 
 running_horses.reverse()
 
+
 def animated_horse(iterations):
     for i in range(iterations):
-        for j in range(0,5):
-            print(running_horses[j-1])
+        for j in range(0, 5):
+            print(running_horses[j - 1])
             sleep(0.15)
-            os.system('clear')
+            os.system("clear")
+
 
 animated_horse(10)
