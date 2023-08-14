4cd2a296b98992af771c3d4f58dfab96b0085ba7
tnosek <tnosek@student.wszib.edu.pl>
2023-08-14 15:13:12 +0200

    creating classes for player and race

---
+++ horse_race/entity_player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self, fname, budget):
+        self.fname = fname
+        self.budget = budget
+
+    def update_budget(self):
+        pass
+
+    def bet(self, horse_name, amount):
+        pass
+        # chosen_horse = horse_name
+        # bet_amount = amount
+
+        # if chosen_horse == winning_horse:
+        #     bet_amount *= 2
+        # else:
+        #     bet_amount /= 2
