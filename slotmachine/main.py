a7732bf2c4cdf41acda6cde22b800986c4db57f4
tnosek <tnosek@student.wszib.edu.pl>
2023-08-17 22:03:12 +0200

    adding progress on slotmachine

--- slotmachine/main.py
+++ slotmachine/main.py
@@ -4,6 +4,56 @@ MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
+ROWS = 3
+COLS = 3
+
+symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
+
+symbol_value = {"A": 5, "B": 4, "C": 3, "D": 2}
+
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+
+        else:
+            winnings += values[symbol] * bet
+
+    return winnings
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row])
+
 
 def deposit():
     while True:
@@ -63,6 +113,9 @@ def main():
     print(
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}"
     )
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
