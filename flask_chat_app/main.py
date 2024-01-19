b993e91a171bbd824ff76aed388d83321825ac27
tnosekk <nosektomek@gmail.com>
2024-01-19 21:58:08 +0100

    made some progress on flask_chat_app, mainly added templates and functions for handling user errors

--- flask_chat_app/main.py
+++ flask_chat_app/main.py
@@ -9,9 +9,49 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "key"
 socketio = SocketIO(app)
 
+rooms = {}
+
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+    return code
+
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template(
+                "home.html", error="Please enter a name.", code=code, name=name
+            )
+        if join != False and not code:
+            return render_template(
+                "home.html", error="Please enter a room code.", code=code, name=name
+            )
+
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template(
+                "home.html", error="Room does not exist", code=code, name=name
+            )
+        session["room"] = room
+        session["name"] = name
+
     return render_template("home.html")
 
 
